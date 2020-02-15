957227cdd8bb71a3f94f12ba8a63e9b4c2e5ab80
Markese Straughter <Kese08@hotmail.com>
2020-02-15 11:03:30 -0800

    spell check

--- main.js
+++ main.js
@@ -1,39 +1,29 @@
 
 alert('Hey, lets test your IQ!')
 var Questionone=prompt('If you drop a yellow hat in the Red Sea, what does it become?')
-
-
 if(Questionone=='Wet')
 {
 alert('GOOD JOB! YOURE SMARTER THAN I THOUGHT')
 }
-
-else
-alert   ('I WONT JUDGE YOU BUT YOUR WROOOOONG')
+else{
+alert   ('I WONT JUDGE YOU BUT YOUR WROOOOONG')}
     
-
-
 alert('Lets test your IQ one more time')
-
 var Questiontwo=prompt('It looks green, it opens red. what you eat is red, but what you spit out is black. what is it?')
+var gameover=true
 if(Questiontwo=='Watermelon')
-  
 {
    alert('IM PROUD OF YOU')
 }
-
-else
-alert('WHY DIDNT YOU JUST USE GOOGLE! GAME OVER FOR YOU')
-
+else{
+alert('WHY DIDNT YOU JUST USE GOOGLE! GAME OVER FOR YOU')}
+gameover=false
 alert('Last question')
 
+if(gameover==false){
 var Questionthree=prompt('what has to be broken before you can use it')
-
 if(Questionthree=='EGG')
-
-{
 alert('HEY YOU WON! Come enjoy a free meal at the one and only, fast casual restaurant, YOURWAY!')
-}
 
 else
-alert('THATS NOT CORRECT. YOU DIDNT LOSE BUT YOU DIDNT WIN!')
+alert('THATS NOT CORRECT. YOU DIDNT LOSE BUT YOU DIDNT WIN!')}
