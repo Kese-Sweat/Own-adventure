96888591a2106e92fa49c16177c073e55b6fe6cd
Markese Straughter <Kese08@hotmail.com>
2020-02-15 07:18:42 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+alert('Hey, lets test your IG!')
+prompt('If you drop a yellow hat in the Red Sea, what does it become?')
+var IQ1= Wet
+if (IQ1) {'GOOD JOB! Way smarter than i thought'}
+else{ 'I wont judge you'}
+    
+alert('Lets test your IG one more time')
+prompt('It looks green, it opens red. what you eat is read, but what you spit out is black. what is it?')
+var IQ2=Watermelon
+if(IQ2){'Im proud of you'}
+else{ 'All you had to do is use Google!'}
