906ffde4ccbfea4385f6d79127795971084607fc
Markese Straughter <Kese08@hotmail.com>
2020-02-15 09:55:18 -0800

    added questions

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
 
-alert('Hey, lets test your IG!')
-prompt('If you drop a yellow hat in the Red Sea, what does it become?')
-var IQ1= Wet
-if (IQ1) {'GOOD JOB! Way smarter than i thought'}
-else{ 'I wont judge you'}
+alert('Hey, lets test your IQ!')
+var Questionone=prompt('If you drop a yellow hat in the Red Sea, what does it become?')
+
+
+if(Questionone=='Wet')
+{
+alert('GOOD JOB! YOURE SMARTER THAN I THOUGHT')
+}
+
+else
+alert   ('I WONT JUDGE YOU BUT YOUR WROOOOONG')
     
-alert('Lets test your IG one more time')
-prompt('It looks green, it opens red. what you eat is read, but what you spit out is black. what is it?')
-var IQ2=Watermelon
-if(IQ2){'Im proud of you'}
-else{ 'All you had to do is use Google!'}
+
+
+alert('Lets test your IQ one more time')
+
+var Questiontwo=prompt('It looks green, it opens red. what you eat is red, but what you spit out is black. what is it?')
+if(Questiontwo=='Watermelon')
+  
+{
+   alert('IM PROUD OF YOU')
+}
+
+else
+alert('WHY DIDNT YOU JUST USE GOOGLE! GAME OVER FOR YOU')
+
+alert('Last question')
+
+var Questionthree=prompt('what has to be broken before you can use it')
+
+if(Questionthree=='EGG')
+
+{
+alert('HEY YOU WON! Come enjoy a free meal at the one and only, fast casual restaurant, YOURWAY!')
+}
+
+else
+alert('THATS NOT CORRECT. YOU DIDNT LOSE BUT YOU DIDNT WIN!')
